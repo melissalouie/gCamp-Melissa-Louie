4735fe9fb85c7714a6e7f064f96cce03dcf37ae9
Melissa Louie <melissalouie@Melissas-MacBook-Pro.local>
2015-01-16 14:52:28 -0700

    add about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def terms
   end
+
+  def about
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
