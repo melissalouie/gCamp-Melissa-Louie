f4c7f59477d210b2a748ea7790e64a9eabeab397
Melissa Louie <melissalouie@Melissas-MacBook-Pro.local>
2015-01-26 11:03:04 -0700

    changed local database to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150122210857) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
     t.datetime "created_at",  null: false
