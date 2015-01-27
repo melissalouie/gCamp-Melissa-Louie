9c88f76071c59c27dc65284acdcdb4e3e4a1a4cf
Melissa Louie <melissalouie@Melissas-MacBook-Pro.local>
2015-01-27 13:50:34 -0700

    CRUD Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127200238) do
+ActiveRecord::Schema.define(version: 20150127202910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,13 +22,6 @@ ActiveRecord::Schema.define(version: 20150127200238) do
     t.datetime "updated_at",  null: false
     t.date     "due_date"
     t.boolean  "completed"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string  "name",          limit: 255
-    t.string  "email",         limit: 255
-    t.boolean "admin",                     default: false, null: false
-    t.date    "date_of_birth"
   end
 
 end
