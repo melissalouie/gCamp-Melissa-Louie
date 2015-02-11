233110374a0eed5246d9e98f4805c7d274f6fe6d
Melissa Louie <melissalouie@Melissas-MacBook-Pro.local>
2015-02-11 16:02:10 -0700

    add edit view for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,14 +12,31 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(project_params)
     if @project.save
+      flash[:notice] = "Project successfully created."
       redirect_to projects_path
     else
       render :new
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      flash[:notice] = "Project successfully updated."
+      redirect_to project_path(@project)
+    else
+      render :edit
+    end
+  end
+
+  def show
+  end
+
   def destroy
     @project.destroy
+    flash[:notice] = "Project successfully deleted."
     redirect_to projects_path
   end
 
