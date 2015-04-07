e68b5e054e9dfdc701955492668f436272e8f6e5
Melissa Louie <melissalouie@Melissas-MacBook-Pro.local>
2015-04-07 15:54:57 -0600

    add dropdown nav for signed in users

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,5 +1,5 @@
 class Membership < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
-  validates :user_id, presence: :true, uniqueness: true
+  validates :user_id, presence: true, uniqueness: {scope: :project_id, message: "has already been added to this project."}
 end
