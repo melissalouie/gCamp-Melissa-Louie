1031f080fe097ef68522f8cdfc19671323554d64
Melissa Louie <melissalouie@Melissas-MacBook-Pro.local>
2015-04-08 15:24:29 -0600

    add admin to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407192652) do
+ActiveRecord::Schema.define(version: 20150408212333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20150407192652) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "password_digest"
+    t.boolean  "admin"
   end
 
   add_foreign_key "comments", "tasks"
