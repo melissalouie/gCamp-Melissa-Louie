bfe95640eafe888a12bb37bc1499b7b1efa6a47d
Melissa Louie <melissalouie@Melissas-MacBook-Pro.local>
2015-04-10 15:46:26 -0600

    fix project spec test

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Users can CRUD projects' do
   before :each do
-    User.create(first_name: 'Missy', last_name: 'Louie', email: 'missy@mail.com', password: 'password')
+    User.create(first_name: 'Missy', last_name: 'Louie', email: 'missy@mail.com', password: 'password', admin: true)
     visit '/signin'
     fill_in 'email', with: 'missy@mail.com'
     fill_in 'password', with: 'password'
@@ -20,13 +20,21 @@ describe 'Users can CRUD projects' do
   end
 
   scenario 'User can edit a project' do
+    visit '/projects'
+    within('table') do
+      click_on 'First Project'
+    end
     click_on 'Edit'
-    fill_in 'project[name]', with: 'Second Project'
+    fill_in 'project[name]', with: 'Edited Project'
     click_on 'Update Project'
-    expect(page).to have_content('Second Project')
+    expect(page).to have_content('Edited Project')
   end
 
   scenario 'User can delete a project' do
+    visit '/projects'
+    within('table') do
+      click_on 'First Project'
+    end
     click_on 'Delete'
     expect(page).to have_no_content('First Project')
   end
