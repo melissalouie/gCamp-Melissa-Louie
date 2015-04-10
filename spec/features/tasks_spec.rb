1137067daf767037af4101b954fba0234d4a1429
Melissa Louie <melissalouie@Melissas-MacBook-Pro.local>
2015-04-10 15:56:08 -0600

    fix tasks spec test

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 describe 'Users can CRUD tasks' do
   before :each do
-    User.create(first_name: 'Missy', last_name: 'Louie', email: 'missy@mail.com', password: 'password')
+    User.create(first_name: 'Missy', last_name: 'Louie', email: 'missy@mail.com', password: 'password', admin: true)
     visit '/signin'
     fill_in 'email', with: 'missy@mail.com'
     fill_in 'password', with: 'password'
     within('form') do
       click_on 'Sign In'
+    end
     @task = Task.create(description: "First Task", due_date: '2015-03-20', project_id: 1)
     @project = Project.create(name: 'First Project')
-    end
   end
 
   scenario 'User can create Task' do
