7bc04a6e17f7fbecb3542060c7a048439c7f7373
Melissa Louie <melissalouie@Melissas-MacBook-Pro.local>
2015-04-16 10:27:03 -0600

    add to tasks controller spec

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -8,32 +8,29 @@ RSpec.describe ProjectsController, type: :controller do
       before(:each) do
         user = User.create(first_name: 'Melissa', last_name: 'Louie', email: 'melissa@mail.com', password: 'password', admin: 'true')
         session[:user_id] = user.id
+        @project = Project.create(name: 'Test Project')
       end
 
       it "creates the requested project" do
-        project = Project.create(name: 'Test Project')
-        post :create, id: project.id, :project => {id: project.id, name: 'Test Project'}
-        expect(project.name).to eq('Test Project')
+        post :create, id: @project.id, :project => {id: @project.id, name: 'Test Project'}
+        expect(@project.name).to eq('Test Project')
+      end
+
+      it "renders the show view" do
+        get :show, id: @project.id
+        expect(response).to render_template(:show)
       end
 
       it "updates the requested project" do
-        project = Project.create(name: 'Test Project')
-        put :update, id: project.id, :project => {id: project.id, name: 'Updated Project'}
-        project.reload
-        expect(project.name).to eq('Updated Project')
+        put :update, id: @project.id, :project => {id: @project.id, name: 'Updated Project'}
+        @project.reload
+        expect(@project.name).to eq('Updated Project')
       end
 
       it "deletes the requested project" do
-        project = Project.create(name: 'Test Project')
         projects = Project.count
-        delete :destroy, id: project.id
+        delete :destroy, id: @project.id
         expect(Project.count).to eq(projects - 1)
-      end
-
-      it "renders show view" do
-        project = Project.create(name: 'Test Project')
-        get :show, id: project.id
-        expect(response).to render_template(:show)
       end
     end
   end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -4,23 +4,33 @@ RSpec.describe TasksController, type: :controller do
 
   describe "PUT #update" do
     context "with valid params" do
-      it "updates the requested task" do
+
+      before(:each) do
         user = User.create(first_name: 'Melissa', last_name: 'Louie', email: 'melissa@mail.com', password: 'password', admin: 'true')
         session[:user_id] = user.id
-        project = Project.create(name: 'Test Project')
-        task = Task.create(description: 'Test Task', due_date: '2015/04/20')
-        put :update, project_id: project.id, id: task.id, :task => {id: task.id, description: 'Updated Task'}
-        task.reload
-        expect(task.description).to eq('Updated Task')
+        @project = Project.create(name: 'Test Project')
+        @task = Task.create(description: 'Test Task', due_date: '2015/04/20')
+      end
+
+      it "creates the requested task" do
+        post :create, project_id: @project.id, id: @task.id, :task => {id: @task.id, description: 'Test Task'}
+        expect(@task.description).to eq('Test Task')
+      end
+
+      it "renders the show view" do
+        get :show, project_id: @project.id, id: @task.id
+        expect(response).to render_template(:show)
+      end
+
+      it "updates the requested task" do
+        put :update, project_id: @project.id, id: @task.id, :task => {id: @task.id, description: 'Updated Task'}
+        @task.reload
+        expect(@task.description).to eq('Updated Task')
       end
 
       it "deletes the requested task" do
-        user = User.create(first_name: 'Melissa', last_name: 'Louie', email: 'melissa@mail.com', password: 'password', admin: 'true')
-        session[:user_id] = user.id
-        project = Project.create(name: 'Test Project')
-        task = Task.create(description: 'Test Task', due_date: '2015/04/20')
         tasks = Task.count
-        delete :destroy, project_id: project.id, id: task.id
+        delete :destroy, project_id: @project.id, id: @task.id
         expect(Task.count).to eq(tasks - 1)
       end
 
