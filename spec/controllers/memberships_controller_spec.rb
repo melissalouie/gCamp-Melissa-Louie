4eb9d7cb6d118918d408e149c4e1f9dadccdaa5d
Melissa Louie <melissalouie@Melissas-MacBook-Pro.local>
2015-04-16 10:50:23 -0600

    add memberships controller spec

---
+++ spec/controllers/memberships_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe MembershipsController, type: :controller do
+
+  context "with valid params" do
+
+    before(:each) do
+      @user = User.create(first_name: 'Melissa', last_name: 'Louie', email: 'melissa@mail.com', password: 'password', admin: true)
+      @user_2 = User.create(first_name: 'Buddy', last_name: 'Louie', email: 'buddy@mail.com', password: 'password', admin: true)
+      session[:user_id] = @user.id
+      @project = Project.create(name: 'Test Project')
+      @membership = Membership.create(user_id: @user.id, project_id: @project.id, role: true)
+    end
+
+    it "creates the requested membership" do
+      post :create, project_id: @project.id, :membership => {id: @membership.id, user_id: @user.id, role: true}
+      expect(@membership.role).to eq(true)
+    end
+    
+    # it "updates the requested membership" do
+    #   put :update, project_id: @project.id, id: @membership.id, :membership => {id: @membership.id, user_id: @user_2.id }
+    #   @membership.reload
+    #   expect(@membership.user_id).to eq(@user_2.id)
+    # end
+
+    it "deletes the requested membership" do
+      memberships = Membership.count
+      delete :destroy, project_id: @project.id, id: @membership.id
+      expect(Membership.count).to eq(memberships - 1)
+    end
+
+  end
+
+end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,37 +2,36 @@ require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
 
-  describe "PUT #update" do
-    context "with valid params" do
+  context "with valid params" do
 
-      before(:each) do
-        user = User.create(first_name: 'Melissa', last_name: 'Louie', email: 'melissa@mail.com', password: 'password', admin: 'true')
-        session[:user_id] = user.id
-        @project = Project.create(name: 'Test Project')
-      end
-
-      it "creates the requested project" do
-        post :create, id: @project.id, :project => {id: @project.id, name: 'Test Project'}
-        expect(@project.name).to eq('Test Project')
-      end
-
-      it "renders the show view" do
-        get :show, id: @project.id
-        expect(response).to render_template(:show)
-      end
-
-      it "updates the requested project" do
-        put :update, id: @project.id, :project => {id: @project.id, name: 'Updated Project'}
-        @project.reload
-        expect(@project.name).to eq('Updated Project')
-      end
-
-      it "deletes the requested project" do
-        projects = Project.count
-        delete :destroy, id: @project.id
-        expect(Project.count).to eq(projects - 1)
-      end
+    before(:each) do
+      user = User.create(first_name: 'Melissa', last_name: 'Louie', email: 'melissa@mail.com', password: 'password', admin: 'true')
+      session[:user_id] = user.id
+      @project = Project.create(name: 'Test Project')
     end
+
+    it "creates the requested project" do
+      post :create, id: @project.id, :project => {id: @project.id, name: 'Test Project'}
+      expect(@project.name).to eq('Test Project')
+    end
+
+    it "renders the show view" do
+      get :show, id: @project.id
+      expect(response).to render_template(:show)
+    end
+
+    it "updates the requested project" do
+      put :update, id: @project.id, :project => {id: @project.id, name: 'Updated Project'}
+      @project.reload
+      expect(@project.name).to eq('Updated Project')
+    end
+
+    it "deletes the requested project" do
+      projects = Project.count
+      delete :destroy, id: @project.id
+      expect(Project.count).to eq(projects - 1)
+    end
+
   end
 
 end
